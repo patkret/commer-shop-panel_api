8d6cbb400550c9d27dd0a1a289ee8fcf7528881e
katarzyna.sawis <katarzyna.sawis@gmail.com>
2018-05-11 12:13:30 +0200

    OrderStatus

---
+++ database/migrations/2018_05_11_092816_create_order_statuses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('type');
+            $table->boolean('active');
+            $table->string('tag_color');
+            $table->boolean('email_notification');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_statuses');
+    }
+}
