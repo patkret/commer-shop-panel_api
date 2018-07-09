554922c6e89f9c51dcf8677bf6a6bc74150056e5
dyktek <a3j6exm@gmail.com>
2018-07-09 13:53:21 +0200

    import

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,9 @@ use Illuminate\Http\Request;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+
+Route::get('/import', 'ImportController@index');
+
 Route::group([
 //	'middleware' => 'acl'
 	'middleware' => 'jwt'
